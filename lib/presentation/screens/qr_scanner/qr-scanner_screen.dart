2f613c0cb7b3bf7ca078261ef3d5a261b8455602
Jose Baptista <jbaptista.dev@gmail.com>
2025-02-20 19:00:24 -0400

    Add QR scanner UI with custom button and instructional text

--- lib/presentation/screens/qr_scanner/qr-scanner_screen.dart
+++ lib/presentation/screens/qr_scanner/qr-scanner_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:qr_scanner_app/presentation/blocs/auth/global_scope/auth_bloc.dart';
+import 'package:qr_scanner_app/presentation/widgets/shared/custom_filled_button.dart';
 
 class QRScannerScreen extends StatelessWidget {
   const QRScannerScreen({super.key});
@@ -16,6 +17,25 @@ class QRScannerScreen extends StatelessWidget {
           IconButton(
               onPressed: () => logout(), icon: Icon(Icons.exit_to_app_sharp))
         ],
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 30,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox(),
+              Text('Aqui podras escanear tus QR',
+                  style: TextStyle(fontSize: 40)),
+              CustomFilledButton(
+                text: 'Escanear QR',
+                onPressed: () {},
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
